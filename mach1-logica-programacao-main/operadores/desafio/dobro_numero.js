3f1a9cd0fa64df45b31292b5b6b89b765243354b
muellerviana <muellerviana@gmail.com>
2022-07-19 03:02:53 -0300

    ex tempo viagem

--- mach1-logica-programacao-main/operadores/desafio/dobro_numero.js
+++ mach1-logica-programacao-main/operadores/desafio/dobro_numero.js
@@ -2,11 +2,21 @@
 Fazer um programa que leia um numero usando o prompt, calcule e infome o dobro desse número.
 */
 
+// const prompt = require('prompt-sync')();
+
+// console.log("Digite o número");
+// let numero = Number(prompt()); //obrigando o dado a vir no padrão numérico 
+// let dobro = numero * 2;
+
+// console.log("O dobro do número: ");
+// console.log(dobro);
+
+
 const prompt = require('prompt-sync')();
 
-console.log("Digite o número");
-let numero = Number(prompt()); //obrigando o dado a vir no padrão numérico 
-let dobro = numero * 2;
+console.log("Digite um número")
 
-console.log("O dobro do número: ");
-console.log(dobro);
+let num = Number(prompt()); 
+let dobro = num * 2
+
+console.log(`O dobro é ${dobro}`)
--- mach1-logica-programacao-main/operadores/desafio/duracao_viagem.js
+++ mach1-logica-programacao-main/operadores/desafio/duracao_viagem.js
@@ -8,15 +8,26 @@ Numero de horas: 4
 Total de horas: 52
 */
 
+// const prompt = require('prompt-sync')();
+
+// console.log("Número de dias da viagem: ");
+// let nDias = Number(prompt()); //3 * 24
+
+// console.log("Número de horas da viagem: ");
+// let nHoras = Number(prompt());// +5
+
+// let totalViagemHoras = nDias * 24 + nHoras;
+
+// console.log("Total da viagem: ");
+// console.log(totalViagemHoras + " horas");
+
+
 const prompt = require('prompt-sync')();
 
-console.log("Número de dias da viagem: ");
-let nDias = Number(prompt()); //3 * 24
+console.log("Digite o numero de dias");
+var dias = Number(prompt());
+console.log("digite o numero de horas");
+var horas = Number(prompt());
 
-console.log("Número de horas da viagem: ");
-let nHoras = Number(prompt());// +5
-
-let totalViagemHoras = nDias * 24 + nHoras;
-
-console.log("Total da viagem: ");
-console.log(totalViagemHoras + " horas");
+var total = (dias * 24) + horas;
+console.log(`total de horas ${total}`);
